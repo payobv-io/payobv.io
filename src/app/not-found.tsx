e0e73995f2a3d89e8e66a6ea9ecde95403c43861
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-08-14 21:59:14 +0900

    Added: Not-found screen

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+  const router = useRouter();
+  return (
+    <div className="grid h-screen px-4 bg-white place-content-center">
+      <div className="text-center">
+        <h1 className="font-black text-gray-200 text-9xl">401</h1>
+
+        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Unauthroized!
+        </p>
+
+        <p className="mt-4 text-gray-500">This page could not be found.</p>
+
+        <Button
+          className="mt-4"
+          onClick={() => {
+            router.replace('/');
+          }}
+        >
+          Home
+        </Button>
+      </div>
+    </div>
+  );
+}
