f9cb1cd3aa255d2b63d434965f5780444dba79b5
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-08-14 21:46:58 +0900

    Added: Middleware configuration

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+'use server';
+
+import { getToken } from 'next-auth/jwt';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const { pathname } = req.nextUrl;
+
+  if (token && pathname === '/') {
+    return NextResponse.redirect(new URL('/profile', req.url));
+  }
+
+  if (!token && pathname === '/profile') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = { matcher: ['/', '/profile'] };
