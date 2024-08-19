487bbaf0e4c784062cd3c51e3c4d6b9ae8420048
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-08-19 13:34:15 +0900

    Added: Basic functionality of connection the contract

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,11 @@
 import AccountListFeature from '@/components/account/account-list-feature';
+import ContractInteraction from '@/components/ContractInteraction';
 
 export default function Page() {
   return (
     <>
       <AccountListFeature />
+      <ContractInteraction />
     </>
   );
 }
