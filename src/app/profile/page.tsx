e30fe4fd73c4116a05333d12c3d8e9d4f8137a08
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-08-24 00:50:30 +0900

    Removed: Contract interaction component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,9 @@
 import AccountListFeature from '@/components/account/account-list-feature';
-import ContractInteraction from '@/components/ContractInteraction';
 
 export default function Page() {
   return (
     <>
       <AccountListFeature />
-      <ContractInteraction />
     </>
   );
 }
