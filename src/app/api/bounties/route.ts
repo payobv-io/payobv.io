bf35bff94705c50db53af82f0b2597235a61ef6d
Ashwin Anil <ashwinrockon123@gmail.com>
2024-08-28 00:46:49 +0900

    feat: Added Route to check if an issue number has bounty

--- src/app/api/bounties/route.ts
+++ src/app/api/bounties/route.ts
@@ -2,6 +2,8 @@ import { db } from '@/db/db';
 import { bountyCreateSchema } from '@/lib/validations';
 import { NextRequest, NextResponse } from 'next/server';
 
+
+// TODO: Use try/catch block to catch errors
 export async function POST(req: NextRequest) {
   const responseData = await req.json();
   const parsedResponse = bountyCreateSchema.safeParse(responseData);
@@ -42,4 +44,45 @@ export async function POST(req: NextRequest) {
     { message: "Bounty Set" }, 
     { status: 200 }
   );
+}
+
+export async function GET(req: NextRequest) {
+  const params = req.nextUrl.searchParams;
+  const issueNumber = params.get('issueNumber');
+
+  if(!issueNumber || isNaN(parseInt(issueNumber))){
+    return NextResponse.json(
+      { message: "Invalid data" }, 
+      { status: 400 }
+    );
+  }
+
+  try {
+    const bounty = await db.bounty.findUnique({
+      where: {
+        issueNumber: parseInt(issueNumber)
+      },
+      select: {
+        amount: true,
+      }
+    })
+  
+    if(!bounty){
+      return NextResponse.json({
+        isBounty: false,
+        bounty: null
+      });
+    }
+    
+    console.log("Bounty Comment: ", bounty);
+    return NextResponse.json({
+      isBounty: true,
+      bounty: bounty.amount
+    });
+  } catch (error) {
+    console.error("Error checking bounty comment: ", error);
+    return NextResponse.json(
+    { message: "Error checking bounty comment" }, 
+    { status: 500 });
+  }
 }
