5507436727107c7f7161885c64dc97487d9a289b
Ashwin Anil <ashwinrockon123@gmail.com>
2024-08-27 21:28:07 +0900

    feat: Bounty Card also includes info abt money escrowed
    - Changed Design
    - Added Subtext to other cards

--- src/components/ui/dashboard/card-wrapper.tsx
+++ src/components/ui/dashboard/card-wrapper.tsx
@@ -1,4 +1,4 @@
-import { getPaidBountyDetails, totalRepositories } from "@/lib/data"
+import { getPaidBountyDetails, getTotalMoneySpent, totalRepositories } from "@/lib/data"
 import { BookIcon, CheckCircleIcon, DollarSignIcon, UsersIcon } from "lucide-react"
 import StatCard from "./stat-card"
 
@@ -10,34 +10,42 @@ export default async function CardWrapper({
   userId
 }: CardWrapperProps) {
   const paidBountyDetails = await getPaidBountyDetails(userId)
+  const totalMoneySpent = await getTotalMoneySpent(userId)
   const totalRepostories = await totalRepositories(userId)
 
   return (
     <div className="grid gap-6 mb-8 md:grid-cols-3">
       <StatCard 
         index={1}
-        title="Total Bounty Paid"
-        value={`${paidBountyDetails.totalAmount} SOL`}
+        title="Bounty"
+        value={`${totalMoneySpent} SOL`}
+        details={{
+          paid: paidBountyDetails.totalAmount,
+          escrowed: totalMoneySpent - paidBountyDetails.totalAmount
+        }}
         icon={<DollarSignIcon className="h-6 w-6 text-gray-400" />}
       />
       <div className="grid gap-6 md:grid-cols-3 col-span-2">
         <StatCard 
           index={2}
-          title="Total Issues Solved"
+          title="Issues"
           value={paidBountyDetails.totalBounties.toString()}
           icon={<CheckCircleIcon className="h-6 w-6 text-gray-400" />}
+          subText="Issues resolved"
         />
         <StatCard 
           index={3}
-          title="Total Repositories"
+          title="Repositories"
           value={totalRepostories.toString()}
           icon={<BookIcon className="h-6 w-6 text-gray-400" />}
+          subText="Repositories managed by you"
         />
         <StatCard 
           index={3}
-          title="Total Contributors"
+          title="Contributors"
           value={paidBountyDetails.totalBounties.toString()}
           icon={<UsersIcon className="h-6 w-6 text-gray-400" />}
+          subText="Received bounties"
         />
       </div>
     </div>
