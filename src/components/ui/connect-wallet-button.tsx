14c12eb55f6a5e8f583ba975137866c99e48f162
Ashwin Anil <ashwinrockon123@gmail.com>
2024-08-29 00:43:04 +0900

    feat: Added a wallet connect button in the Maintainer Dashboard

---
+++ src/components/ui/connect-wallet-button.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { ArrowRightIcon, WalletIcon } from "lucide-react"
+import { Button } from "./button"
+import { useEffect } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
+import { addWallet } from "@/lib/actions";
+
+export default function ConnectWalletButton() {
+  const { wallet, connected, connect, disconnect, publicKey } = useWallet();
+  const { setVisible } = useWalletModal();
+  const handleConnectWallet = async () => {
+    if (connected) {
+      await disconnect();
+    } else if (wallet) {
+      await connect();
+    } else {
+      setVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    const handleWalletConnection = async () => {
+      if (connected) {
+        await addWallet({
+          publicAddress: publicKey!.toString(),
+        });
+      }
+    };
+
+    handleWalletConnection();
+  }, [connected, publicKey]);
+
+  return (
+    <Button
+      variant="outline"
+      disabled={connected}
+      onClick={handleConnectWallet}
+      className="flex items-center gap-x-2"
+    >
+      <WalletIcon className="h-5 w-5" />
+      <span>
+        {connected ? 'Wallet Connected' : 'Connect Wallet'}
+      </span>
+      {
+        connected ? 
+          <span className="top-1/2 transform-translate-y-1/2 w-2 h-2 bg-green-500 rounded-full"></span>
+        : undefined
+      }
+    </Button>
+  )
+}
