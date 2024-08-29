28f3ed1199be3241232ef29ce9d16c4b3f052e99
Ashwin Anil <ashwinrockon123@gmail.com>
2024-08-29 14:03:19 +0900

    fix: Moved Base url to env and endpoints to constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
 import { BadgeProps } from '@/components/ui/badge';
 import { BountyStatus } from '@prisma/client';
 
+export const GITHUB_APP_API_BASE_URL = `${process.env.NEXT_PUBLIC_GITHUB_APP_SERVER_URL}/payobvio-github-app`;
+export const SOLANA_NETWORK = process.env.NEXT_PUBLIC_SOLANA_NETWORK as string;
+
 export const bountyStatusDetails: {
   [key in BountyStatus]: {
     variant: BadgeProps['variant'];
@@ -28,3 +31,8 @@ export const bountyStatusDetails: {
     label: 'REJECTED',
   },
 };
+
+export const ENDPOINTS = {
+  ESCROW: '/escrow',
+  ESCROW_RELEASED: '/escrow-released',
+}
